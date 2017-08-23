22c427d6d59592d90fe03b391bb2bc6d2035c902
Federico Mieres <magno.f.mieres@gmail.com>
2017-08-23 11:49:44 -0300

    Reestructuración bot - comando desconocido comando help comando unit_names

--- bot/main.py
+++ bot/main.py
@@ -9,19 +9,32 @@ token =  sys.argv[1]
 def find_by_name(name):
   client = MongoClient('mongodb://localhost:27017/')
   unit = client.ffbe.units.find_one({"name" : name })
+  print unit
+  print client.ffbe.units.find_one()
   client.close
   return unit
 
 bot = telebot.TeleBot(token)
 
-# @bot.message_handler(commands=['start', 'help'])
-# def send_welcome(message):
-#   bot.reply_to(message, "Howdy, how are you doing?")
+@bot.message_handler(commands=['help'])
+def help(message):
+  bot.send_message(message.chat.id, 'I am a ffbe info bot, available commands: unit_names')
+
+@bot.message_handler(commands=['unit_names'])
+def unit_names(message):
+  try :
+    name = message.text.split(' ',1)[1]
+    print name
+    unit = find_by_name(name)
+    bot.reply_to(message, json.dumps(unit["names"]))
+  except Exception as e :
+    bot.reply_to(message, 'unit name ' + name + ' not found')
+
 
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-  unit = find_by_name(message.text)
-  bot.reply_to(message, json.dumps(unit["names"]))
+def default_messgae_unknown(message):
+  bot.reply_to(message, 'unknown command: ' + message.text)
+
 
 bot.polling()
 
