0731ab22c9d2d7fdb573390eccff86b5d74f71a9
Federico Mieres <magno.f.mieres@gmail.com>
2017-09-16 17:54:13 -0300

    actuqalizado bot con logging y repo

--- bot/main.py
+++ bot/main.py
@@ -1,52 +1,16 @@
+import logging
 import telebot
 import json
 import sys
-from pymongo import MongoClient
 from pprint import pprint
+from repository import Repository
+from loggingInterface import Log, STOUTHandler
 
 token =  sys.argv[1]
-
-
-def find_by_name(name):
-  client = MongoClient('mongodb://localhost:27017/')
-
-  #unit = client.ffbe.units.find_one({"name" : name })
-  units = client.ffbe.units.aggregate([
-
-    {'$match': {"name": name}},
-    
-    {'$unwind':"$skills"},
-    
-    {
-      '$lookup': {
-        'from': 'skills', 
-        'localField': 'skills.id', 
-        'foreignField': 'id', 
-        'as': 'skill_info'
-      }
-    },
-    
-    {'$unwind':"$skill_info"},
-    
-    {'$group':{
-        '_id': '$_id',
-        'id': {'$first':'$id'},
-        'name': {'$first':'$name'},
-        'names': {'$first':'$names'},
-        'skills': {
-            '$push': '$skill_info.name'
-        }
-    }},
-
-  ])
-
-  unit = list(units)[0]
-  del unit['_id']
-
-  client.close
-  return unit
-
 bot = telebot.TeleBot(token)
+repo = Repository()
+log = Log(STOUTHandler())
+log.info("Setting up bot")
 
 @bot.message_handler(commands=['help'])
 def help(message):
@@ -56,9 +20,10 @@ def help(message):
 def unit_names(message):
   try :
     name = message.text.split(' ',1)[1]
-    unit = find_by_name(name)
+    unit = repo.find_unit_by_name(name)
     bot.reply_to(message, json.dumps(unit["names"]))
-  except Exception:
+  except Exception as e:
+    log.error("Exception:", exc_info=True)
     bot.reply_to(message, "unit name '" + name + "' not found")
 
 @bot.message_handler(commands=['unit'])
@@ -66,15 +31,15 @@ def unit(message):
   try :
     name = message.text.split(' ',1)[1]
     if name.startswith("Zarg"): name = 'Zargabaath'
-    unit = find_by_name(name)
+    unit = repo.find_unit_by_name(name)
     bot.reply_to(message, json.dumps(unit))
   except Exception:
+    log.error("Exception:", exc_info=True)
     bot.reply_to(message, "unit name '" + name + "' not found")
 
 @bot.message_handler(func=lambda m: True)
 def default_message_unknown(message):
   bot.reply_to(message, 'unknown command: ' + message.text)
 
-
+log.info("Bot ready, polling")
 bot.polling()
-
