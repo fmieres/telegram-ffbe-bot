436b76fb0be37bdd3b31972c985cbdfafa5a1d54
federico mieres <magno.f.mieres@gmail.com>
2018-05-13 18:47:44 -0300

    cambios en el formato de unidad

--- src/printers/unit.js
+++ src/printers/unit.js
@@ -1,10 +1,9 @@
 module.exports = { found, suggestions, not_found }
 
 function found(unit){
-  const { id, name, skills, sex, game, rarity_min, rarity_max, job, names, stats } = unit
-  return `<b> ${name} </b> (${rarity_min}&#9734 - ${rarity_max}&#9734)
-  <a href="https://exviusdb.com/static/img/assets/unit/unit_ills_${id}.png">${sex} ${game} ${job}</a><a href="https://exvius.gamepedia.com/${name}">&#x1f517</a>
-  ` + stats_formatter(stats) + skills_formatter(skills)
+  return main_formatter(unit) +
+    stats_formatter(unit.stats) +
+    skills_formatter(unit.skills)
 }
 
 function suggestions(name, suggestions){
@@ -16,15 +15,22 @@ function not_found(name){
 }
 
 function skills_formatter(skills = []){
-  return `<b>Skills: </b>` + '\n' +
+  return `  <b>Skills: </b>` + '\n' +
     skills.map( name => `<code>  -</code><a href="https://exvius.gamepedia.com/${name}">${name}</a>` ).join('\n')
 }
 
 function stats_formatter(stats){
-  return `<b>Stats: </b> (at max lvl)` + '\n' +
+  console.log(stats)
+  return `  <b>Stats: </b> (at max lvl)` + '\n' +
     Object.keys(stats).map( key => {
       const [ _, base, pots ] = stats[key]
       const extraSpace = ['MP', 'HP'].includes(key) ? ' ' : ''
       return `<code>  - ${key}${extraSpace} : ${base}(+${pots})</code>`
     }).join('\n') + '\n'
+}
+
+function main_formatter(unit){
+  const { id, name, skills, sex, game, rarity_min, rarity_max, job, names, stats } = unit
+  return `<a href="https://exviusdb.com/static/img/assets/unit/unit_ills_${id}.png">&#xFE0F</a><a href="https://exvius.gamepedia.com/${name}">${name}</a> (${rarity_min}&#9734 - ${rarity_max}&#9734)
+  ${sex} ${game} ${job}` + '\n'
 }
