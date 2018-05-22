8253de3948d6f6b570736967dbd4273d5b5d979c
federico mieres <magno.f.mieres@gmail.com>
2018-05-22 03:42:49 -0300

    Avances

--- src/printers/tmr.js
+++ src/printers/tmr.js
@@ -18,21 +18,55 @@ function for_unit(tmr, is_full){
     : ''
 }
 
-function print_full(tmr){
-  const { name, effects } = tmr
-  return `<b>${name}</b> ${effects.join(' ')}`
-}
 function print(tmr){
-  console.log('log: ',tmr);
-  const { name, effects, stats } = tmr
-  return ` <a href="https://exvius.gamepedia.com/${name}">${name}</a>(${tmr.tmr_type.toLowerCase()})` + '\n' +
-    (!!effects && effects.length > 0 ? `  ${effects.join(' ')}` + '\n' : '' ) + 
-    (!!stats 
-      ? `  ATK:${stats.ATK}` + '\n' 
-      : '' 
-    ) 
+  const { name } = tmr
+  return ` <b>TMR</b>: <a href="https://exvius.gamepedia.com/${name}">${name}</a>` + '\n'
 }
 
-function stats(stats){
-  
+function materia_tmr(){
+  return ''
+}
+
+function equip_tmr({name, effects, stats, type}){
+  const { element_inflict, element_resist, status_inflict, status_resist } = stats 
+  return '' + 
+    (!!effects && effects.length > 0 ? `  ${effects.join(' ')}` + '\n' : '' ) + 
+    `  <b>${type}</b>` + '\n' + 
+    (!!stats                         ? `   ${stats_text(stats)}` + '\n' : '' ) +
+    (!!element_inflict ? tmr_equip_elements_inflict(element_inflict) : '') + 
+    (!!element_resist  ? tmr_equip_elements_resist(element_resist)   : '')
+    // (!!status_inflict ? tmr_equip_status_inflict(status_inflict) : '')
+    // (!!status_resist ? tmr_equip_status_resist(status_resist) : '')
+}
+
+function tmr_equip_elements_resist(obj){
+  return '  <b>Element Resist</b>\n' + 
+    '   ' + Object.keys(obj).reduce((accum, element) => [...accum, `${element}: ${obj[element]}`], []).join(', ') + '\n'
+}
+
+function tmr_equip_elements_inflict(list){
+  return '  <b>Element</b> ' + list.join(', ') + '\n'
+}
+
+const tmr_type_print = {
+  equip : equip_tmr,
+  materia : materia_tmr
+}
+
+function print_full(tmr){
+  const { name, effects, stats, type } = tmr
+  return ` <b>TMR</b>: <a href="https://exvius.gamepedia.com/${name}">${name}</a> (${tmr.tmr_type.toLowerCase()})` + '\n' +
+    tmr_type_print[tmr.tmr_type.toLowerCase()](tmr)
+    // (!!effects && effects.length > 0 ? `  ${effects.join(' ')}` + '\n' : '' ) + 
+    // (!!type <b>${tmr.type}</b>)
+    // (!!stats                         ? `  ${stats_text(stats)}` + '\n' : '' ) 
+}
+
+const main_stats = ['ATK', 'DEF', 'SPR', 'MAG', 'MP', 'HP']
+
+function stats_text(stats){
+  return main_stats.reduce(
+    (list, stat) => stats[stat] > 0 ? [...list, `${stat}:${stats[stat]}`] : list,
+    []
+  ).join() 
 }
